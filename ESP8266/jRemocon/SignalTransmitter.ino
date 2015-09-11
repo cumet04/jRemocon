dbe43f9ce73c080613f9bb0a969f5aa970f5dd72
medalhkr <medalhkr@gmail.com>
2015-09-12 06:07:08 +0900

    extractMessageまでリファクタリング - あとはsend

--- ESP8266/jRemocon/SignalTransmitter.ino
+++ ESP8266/jRemocon/SignalTransmitter.ino
@@ -1,37 +1,9 @@
 
-void IRTransmitter_setup() {
-//     pinMode(LED_PIN, OUTPUT);
-// 
-//     // from http://www.wsnak.com/wsnakblog/?p=4110
-//     // ************ 38KHz ***************
-//     // 16MHz * 1/1 = 16MHz
-//     // T = 1 / 16MHz = 0.0625us
-//     // 逆算 F = 1 / (0.0625 * 208 * 2) = 38.46kHz
-//     pinMode(CARRIER_PIN, OUTPUT);
-// 
-//     // OC1A(PB1/D9) toggle
-//     TCCR1A &= ~(1<<COM1A1);     // 0
-//     TCCR1A |=  (1<<COM1A0);     // 1
-// 
-//     // WGM13-10 = 0100 CTCモード
-//     TCCR1B &= ~(1<<WGM13);      // 0
-//     TCCR1B |=  (1<<WGM12);      // 1
-//     TCCR1A &= ~(1<<WGM11);      // 0
-//     TCCR1A &= ~(1<<WGM10);      // 0
-// 
-//     // ClockSource CS12-CS10 = 001 16MHz / 1 T= 0.0625us
-//     TCCR1B &= ~(1<<CS12);       // 0
-//     TCCR1B &= ~(1<<CS11);       // 0
-//     TCCR1B |=  (1<<CS10);       // 1
-// 
-//     OCR1A = 207;                // コンペア値
-}
-
 // return code: 0 = ok
 //              -1 = format error
 //              -2 = signaldata includes non hex char
 //              -3 = buffer over flow
-int decodeSignalData(String input, unsigned int &width, byte signal[]) {
+int decodeSignalData(String input, unsigned int &width, byte signal[], int size) {
     // read params; width and signal
     String signal_str = extractValue(input, "signal");
     if (signal_str == "") return -1;
@@ -45,8 +17,9 @@ int decodeSignalData(String input, unsigned int &width, byte signal[]) {
         byte b = convertHexcharToByte(signal_str[i]);
         if (b == -1) return -2;
 
-        if (i/2 >= sizeof(signal)) return -3;
-        signal[i/2] = (i%2 == 1) ? b << 4 : b;
+        if (i/2 >= size) return -3;
+        byte add = (i%2 == 1) ? b : b << 4;
+        signal[i/2] += add;
     }
 
     return 0;
@@ -101,12 +74,14 @@ int TEST_convertHexcharToByte() {
 }
 
 
-void emitSignal(unsigned int width, byte signal[], int pin) {
-    for (int n = 0; n < sizeof(signal); n++) {
+void emitSignal(unsigned int width, byte signal[], int size, int pin) {
+    return;
+    analogWriteFreq(38 * 1000);
+    for (int n = 0; n < size; n++) {
         byte single_byte = signal[n];
         for(int i = 7; i >= 0; i--) {
-            int value = (single_byte & (1 << i)) ? HIGH : LOW;
-            digitalWrite(pin, value);
+            int value = (single_byte & (1 << i)) ? PWMRANGE/3 : LOW;
+            analogWrite(pin, value);
             delayMicroseconds(width);
         }
     }
--- ESP8266/jRemocon/jRemocon.ino
+++ ESP8266/jRemocon/jRemocon.ino
@@ -1,21 +1,25 @@
 #include <ESP8266WiFi.h>
 
+#define equal(a,b) (strcmp(a,b) == 0)
 
-const int LED_PIN = 0;
+const int LED_PIN = 2;
 
 const char* Wifi_SSID = "AirPort07934";
 const char* Wifi_PASS = "3101035127901";
+const char* API_PREFIX = "/jRemocon";
 const int Server_Portnum = 8080;
 
-const String HTTP_OK = "200 OK";
-const String HTTP_NOTALLOWED = "405 Method Not Allowed";
-const String HTTP_NOTFOUND = "404 Not Found";
-const String HTTP_ERROR = "500 Internal Server Error";
-const String HTTP_BADREQUEST = "400 Bad Request";
+const char* HTTP_OK = "200 OK";
+const char* HTTP_NOTALLOWED = "405 Method Not Allowed";
+const char* HTTP_NOTFOUND = "404 Not Found";
+const char* HTTP_ERROR = "500 Internal Server Error";
+const char* HTTP_BADREQUEST = "400 Bad Request";
 
 WiFiServer server(Server_Portnum);
 
 void setup() {
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);
     Serial.begin(115200);
     delay(10);
 
@@ -34,144 +38,162 @@ void loop() {
     if (!client) return;
     Serial.println("connection established.");
 
-    String method, param;
-    int result = readRequest(client, method, param);
+    Serial.println("log: readRequest");
+    char request[0x500];
+    if (readRequest(client, request, sizeof(request)) != 0) return;
+    Serial.print("res: request = "); Serial.println(request);
+
+    Serial.println("log: extractMethod");
+    // char method[0x10], param_str[0x1000];
+    char method[0x10], param_str[0x200];
+    int result = extractMethod(request, API_PREFIX, method, param_str, \
+            sizeof(method), sizeof(param_str));
     if (result == -1) {
-        Serial.println("readRequest failed.");
+        respondString(client, HTTP_NOTALLOWED, "unexpected HTTP method");
+        client.stop();
+        return;
     } else if (result == -2) {
-        respondString(client, HTTP_NOTALLOWED, "HTTP method not allowed");
-        Serial.println("readRequest failed: unexpected HTTP method.");
-    } else if (result == 0) {
-        String status = "";
-        String response = "";
-        if (method == "/" || method == "/help") {
-            processRequest_help(status, response);
-        } else if (method == "/send") {
-            processRequest_send(status, response, param);
-        } else {
-            status = HTTP_NOTFOUND;
-            response = "method not found";
-        }
-        respondString(client, status, response);
+        respondString(client, HTTP_NOTFOUND, "API path not found");
+        client.stop();
+        return;
+    } else if (result == -3) {
+        respondString(client, HTTP_ERROR, "buffer over flow");
+        client.stop();
+        return;
     }
+    Serial.print("res: method = "); Serial.println(method);
+    Serial.print("res: param_str = "); Serial.println(param_str);
+
+    Serial.println("log: processRequest");
+    char res_status[0x100], res_message[0x100];
+    if (equal(method, "") || equal(method, "/") || equal(method, "/help")) {
+        Serial.println("log: processRequest_help");
+        processRequest_help(res_status, res_message);
+    }
+    if (equal(method, "/send")) {
+        Serial.println("log: processRequest_send");
+        processRequest_send(param_str, res_status, res_message);
+    }
+    Serial.print("res: res_status = "); Serial.println(res_status);
+    Serial.print("res: res_message = "); Serial.println(res_message);
+
+    Serial.println("log: respondString");
+    respondString(client, res_status, res_message);
+
+    client.stop();
 
     Serial.print("wait for request: ");
     Serial.println(WiFi.localIP());
 }
 
+int extractMethod(char request[], const char* prefix, char method[], char param[],
+                    int size_m, int size_p) {
+    char *p = strtok(request, " ");
+    if (strncmp(p, "GET", 3) != 0) return -1;
 
-int readRequest(WiFiClient client, String &path, String &param) {
-    Serial.println("readRequest");
-    path = "";
-    param = "";
+    p = strtok(NULL, " ");
+    if (strncmp(p, prefix, strlen(prefix)) != 0) return -2;
 
-    // read request
-    String method = readWord(client);
-    if (method == "\r") return -1;
-    if (method != "GET" && method != "POST") return -2;
-    path = readWord(client);
-    if (path == "\r") return -1;
-    Serial.print("HTTP method: "); Serial.println(method);
-    Serial.print("path: "); Serial.println(path);
+    if (strchr(p, '?') == NULL) {
+        if (strlen(p + strlen(prefix)) > size_m -1) return -3;
+        strcpy(method, p +strlen(prefix));
 
-    // read headers
-    Serial.println("other headers:");
-    while (true) {
-        String line = readWord(client); // other headers are ignored
-        if (line == "\r") return -1;
-        if (line == "") break;
-        Serial.println(line);
+        param[0] = '\0';
+    } else {
+        char *mp = strtok(p, "?");
+        if (strlen(mp + strlen(prefix)) > size_m -1) return -3;
+        strcpy(method, mp);
+
+        char *pp = strtok(NULL, "");
+        if (strlen(pp) > size_p-1) return -3;
+        strcpy(param, pp);
     }
-
-    // put param from path, if method is GET
-    if (method == "GET") {
-        int pos = path.indexOf("?");
-        if (pos == -1) param = "";
-        else {
-            param = path.substring(pos+1);
-            path = path.substring(0, pos);
-        }
-    }
-
-    // read body, if method is POST
-    if (method == "POST") {
-        param = readWord(client);
-        if (param == "\r") return -1;
-    }
-
-    Serial.print("path: "); Serial.println(path);
-    Serial.print("param: "); Serial.println(param);
-
     return 0;
 }
 
-String readWord(WiFiClient client) {
-    char buf[0xFFF];
-    int pos = 0;
-    while (client.connected() && pos < sizeof(buf)-1) {
-        if (!client.available()) continue;
-        char c = client.read();
+int readRequest(WiFiClient client, char request[], int size) {
+    while (!client.available()) ; // wait until first byte is reached
 
-        if (c == '\r') continue;
-        if (c == ' ' || c == '\n') {
-            buf[pos] = '\0';
-            return String(buf);
+    int i = 0;
+    while (true) {
+        if (!client.connected()) {
+            Serial.println("readRequest failed: connection closed.");
+            return -1;
         }
-        buf[pos] = c;
-        pos++;
+        if (!client.available()) break;
+
+        request[i] = client.read();
+        i++;
+        if (i == size) {
+            Serial.println("readRequest failed: buffer over flow.");
+            return -2;
+        }
     }
-    return String('\r'); // connection is lost, or buffer over flow
+    request[i] = '\0';
+    return 0;
 }
 
-void processRequest_help(String &status, String &response) {
-    status = HTTP_OK;
-    response = "to be implemented; help message";
+void processRequest_help(char status[], char response[]) {
+    strcpy(status, HTTP_OK);
+    strcpy(response, "to be implemented; help message");
     return;
 }
 
-void processRequest_send(String &status, String &response, String param) {
-    unsigned int width;
-    byte signal[0xFF];
-    switch (decodeSignalData(param.c_str(), width, signal)) {
-        case 0:
-            status = HTTP_OK;
-            response = "send accepted: " + param;
-            break;
-        case -1:
-            status = HTTP_BADREQUEST;
-            response = "invalid parameters";
-            return;
-        case -2:
-            status = HTTP_BADREQUEST;
-            response = "signal has nox-hex char";
-            return;
-        default:
-            status = HTTP_ERROR;
-            response = "decodeSignalData is falied";
-            return;
-    }
+void processRequest_send(char input[], char status[], char response[]) {
+    // String pulse_raw = extractValue(input, "pulse");
+    // if (pulse_raw == "") {
+    //     status = HTTP_BADREQUEST;
+    //     response = "parse pulse failed";
+    //     return;
+    // }
+    // unsigned int pulse = pulse_raw.toInt();
 
-    emitSignal(width, signal, LED_PIN);
+    // String signal_str = extractValue(input, "signal");
+    // if (signal_str == "") {
+    //     status = HTTP_BADREQUEST;
+    //     response = "parse pulse failed";
+    //     return;
+    // }
+
+    // byte signal[0xFF] = {};
+    // int size = sizeof(signal);
+    // // switch (decodeSignalData(signal_str, signal_bytes, size)) {
+    // //     case -1:
+    // //         status = HTTP_BADREQUEST;
+    // //         response = "signal has nox-hex char";
+    // //         return;
+    // //     case -1:
+    // //         status = HTTP_ERROR;
+    // //         response = "decodeSignalData is falied";
+    // //         return;
+    // // }
+    // status = HTTP_OK;
+    // response = "send accepted: " + input;
+
+    // unsigned int time0 = micros();
+    // emitSignal(pulse, signal, size, LED_PIN);
+    // unsigned int time1 = micros();
+    // Serial.println(time1-time0);
 
     return;
 }
 
 
-void respondString(WiFiClient client, String status, String response) {
+void respondString(WiFiClient client, const char *status, const char *response) {
+    Serial.println(response);
+
     // return header
     client.print("HTTP/1.1 ");
     client.println(status);
     client.println("Content-Type: text/plain");
-    if (status == HTTP_NOTALLOWED) client.println("Allow: GET, POST");
+    if (status == HTTP_NOTALLOWED) client.println("Allow: GET");
     client.println("Connection: close");
     client.println();
 
     // return response string
     client.println(response);
-        
-    // connection stop
-    client.stop();
-    
+    client.flush();
+
     return;
 }
 
@@ -231,4 +253,3 @@ void runTest() {
 
     Serial.println("All tests are passed.");
 }
-
